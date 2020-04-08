e8744ffb49ab7cbf2af4ebf36b461bc0fcf857ca
Unknown <surikhin@rambler.ru>
2020-04-08 20:00:04 +0700

    bootstrap example

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,27 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {MatRadioModule} from '@angular/material'; // added
-import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
-import { RadioButtonListQuestionComponent } from './radio-button-list-question/radio-button-list-question.component';
-import { TextQuestionComponent } from './text-question/text-question.component';
-import {HttpClientModule}  from '@angular/common/http';
+import {RadioButtonListQuestionComponent} from './radio-button-list-question/radio-button-list-question.component';
+import {TextQuestionComponent} from './text-question/text-question.component';
+import {HttpClientModule} from '@angular/common/http';
 import {UsersForTestingComponent} from './usersfortesting.component';
-import {RouterModule, Routes } from '@angular/router';
-import {QuestionsComponent}  from './Questions/questions.component';
-import { ErrorService } from './Services/ErrorService';
-import { VYTestService } from './Services/VYTestService';
-import { DisplayErrorComponent } from './display-error/display-error.component';
-
-
+import {RouterModule, Routes} from '@angular/router';
+import {QuestionsComponent} from './Questions/questions.component';
+import {ErrorService} from './Services/ErrorService';
+import {VYTestService} from './Services/VYTestService';
+import {DisplayErrorComponent} from './display-error/display-error.component';
 
 
 const appRoutes: Routes = [
-  {path: 'GeogeUsers', component:UsersForTestingComponent },
-  {path: 'Q', component:QuestionsComponent },
+  {path: 'GeogeUsers', component: UsersForTestingComponent},
+  {path: 'Q', component: QuestionsComponent},
 
-  
+
 ];
 
 @NgModule({
@@ -32,24 +30,25 @@ const appRoutes: Routes = [
     TextQuestionComponent,
     UsersForTestingComponent,
     DisplayErrorComponent
-   
+
   ],
-  
+
   imports: [
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      {enableTracing: true} // <-- debugging purposes only
     )
-   ,
+    ,
     BrowserModule,
     NoopAnimationsModule,
     MatRadioModule, //added
     FormsModule,
     HttpClientModule
   ],
-  providers : [ErrorService,VYTestService],
+  providers: [ErrorService, VYTestService],
   bootstrap: [AppComponent],
- 
+
 })
 
-export class AppModule { }
+export class AppModule {
+}
